cf20ca97e2feba7efeb384609c464c492857f175
Aritra Kumar DattaChaudhury <aritrasiliguri@gmail.com>
2021-08-23 16:37:35 +0530

    SRP principle implementation

---
+++ GlobomanticsHR/src/main/java/hr/logging/ConsoleLogger.java
@@ -0,0 +1,10 @@
+package hr.logging;
+
+public class ConsoleLogger {
+    public void writeInfo(String msg){
+        System.out.println("Info: "+msg);
+    }
+    public  void writeError(String msg,Exception e){
+        System.out.println("Error: "+ msg +";"+ e);
+    }
+}
