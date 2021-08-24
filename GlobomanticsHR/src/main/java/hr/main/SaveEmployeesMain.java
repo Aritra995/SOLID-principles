1aa731b21485c7c61e29ec0d3c7b010bb798584b
Aritra Kumar DattaChaudhury <aritrasiliguri@gmail.com>
2021-08-24 11:37:19 +0530

    Updated main

--- GlobomanticsHR/src/main/java/hr/main/SaveEmployeesMain.java
+++ GlobomanticsHR/src/main/java/hr/main/SaveEmployeesMain.java
@@ -4,9 +4,13 @@ import hr.logging.ConsoleLogger;
 import hr.persistence.EmployeeFileSerializer;
 import hr.persistence.EmployeeRepository;
 import hr.personnel.Employee;
+import hr.taxes.TaxCalculator;
+import hr.taxes.TaxCalculatorFactory;
 
 import java.io.IOException;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class SaveEmployeesMain {
     public static void main(String[] args) {
@@ -17,13 +21,23 @@ public class SaveEmployeesMain {
         List<Employee> employees = repository.findAll();
 
         // Save all
+        Locale locale = new Locale("en","US");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+
+        double totalTax =0;
         for (Employee e : employees){
+            TaxCalculator taxCalculator = TaxCalculatorFactory.create(e);
+            double tax = taxCalculator.calculate(e);
+            String formattedTax = currencyFormatter.format(tax);
             try {
                 repository.save(e);
-                consoleLogger.writeInfo("Employee saved"+e.toString());
+                consoleLogger.writeInfo("Employee saved "+e.toString());
+                consoleLogger.writeInfo(e.getFullName()+" tax: "+formattedTax);
             } catch (IOException exception) {
-                consoleLogger.writeError("Error saving employee",exception);
+                consoleLogger.writeError("Error saving employee", exception);
             }
+            totalTax += tax;
         }
+        consoleLogger.writeInfo("Total tax: "+currencyFormatter.format(totalTax));
     }
 }
